d3862772881c1e86fd6377b43224b9e907af6070
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2024-01-26 00:10:21 +0200

    generic1

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,11 @@
-let some:unknown;
-some = 'Text';
+let some: unknown;
+some = "Text";
+
 let str: string;
-str = some;
+
+if (typeof some === "string" || some instanceof String) {
+  str = some as string;
+} else {
+}
 
 export {};
